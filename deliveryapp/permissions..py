470c8f97d20e553635d702c18f54f1c8b1c945f1
kien luc <luckien9a6@gmail.com>
2021-12-05 21:51:54 +0700

    update

--- deliveryapp/permissions..py
+++ deliveryapp/permissions..py
@@ -1,13 +1,1 @@
-# from rest_framework.permissions import IsAuthenticated, IsAdminUser, OR
-#
-#
-# class PermissionAddAuctionIntoPost(IsAuthenticated):
-#     def has_permission(self, request, view):
-#         return super(PermissionAddAuctionIntoPost, self).has_permission(request, view)\
-#                 and request.user.has_perms(['abaShip.add_auction_into_post'])
-#
-#
-# class PermissionUpDateStatusOrder(IsAuthenticated):
-#     def has_permission(self, request, view):
-#         return super().has_permission(request,view)\
-#             and request.user.has_perm('abaShip.update_status_order')
+
